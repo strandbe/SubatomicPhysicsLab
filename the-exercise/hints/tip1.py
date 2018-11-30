21ce427a34432db5e569042355206d4958bc4c78
Christian Ohm <christian.ohm@cern.ch>
2018-11-30 18:15:34 +0100

    Adjusting also tip[1,2].py

--- the-exercise/hints/tip1.py
+++ the-exercise/hints/tip1.py
@@ -5,7 +5,6 @@ button = widgets.Button(description="Tip event loop")
 display(button)
 
 def on_button_clicked(b):
-    print("[SWE] Ett snabbsätt att loopa över händelser i ett träd är att skriva 'for evt in tree: evt.branch...'")
-    print("[ENG] A quick way to iterate over a tree is to write 'for evt in tree: evt.branch...'")
+    print("A quick way to iterate over a tree is to write 'for evt in tree: evt.branch...'")
 
 button.on_click(on_button_clicked)
